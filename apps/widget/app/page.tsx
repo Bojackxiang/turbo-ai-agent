ffad6e8693d270e0115dc138f619b2d1cfcad6c5
StewieGriffin104 <batmandocode@gmail.com>
2025-11-07 15:20:15 +1100

    feat: refactor Page component to include Suspense for WidgetView rendering

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { use } from "react";
-import { useVapi } from "../modules/hooks/use-vapi";
 import { WidgetView } from "@/modules/ui/views/widget-view";
+import { Suspense } from "react";
 
-interface Props {
-  searchParams: Promise<{
-    orgId: string;
-  }>;
-}
-
-export default function Page({ searchParams }: Props) {
-  return <WidgetView />;
+export default function Page() {
+  return (
+    <Suspense>
+      <WidgetView />
+    </Suspense>
+  );
 }
